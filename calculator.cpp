339a8cc1667de11d0968330db4a6f1020a2c65d9
ifindev <arifin.effendi@mail.ugm.ac.id>
2020-08-16 17:55:24 +0700

    first commit

---
+++ calculator.cpp
@@ -0,0 +1,39 @@
+/********************************************************************
+*	Program kalkulator sederhana untuk empat operasi				*
+*	penjumlahan, pengurangan, pembagian, dan perkalian				*
+*																	*
+* Author: Muhammad Arifin											*
+* Date	: 16 Agustus 2020											*
+********************************************************************/
+
+#include <iostream>
+
+int main() {
+	
+	// Tampilkan judul program
+	std::cout << "========================================================" << std::endl;
+	std::cout << "============= Program Kalkulator Sederhana =============" << std::endl;
+	std::cout << "========================================================" << std::endl;
+
+	// Deklarasikan variabel-variabel yang akan digunakan
+
+	// Tampilkan operasi-operasi yang ada
+
+	// Masukkan pilihan operasi pengguna (1-4)
+
+	/*==============================================================
+	Cek apakah pilihan angka di rentang (1-4)
+	Jika pilihan operasi (1-4), mulai operasi
+	Jika tidak, berikan pesan mohon maaf. Selesaikan program 
+	================================================================*/
+
+	// Evaluasi pilihan operasi
+
+
+
+	// Tampilkan ucapan terimakasih
+
+
+	std::cin.get();
+	return 0;
+}
